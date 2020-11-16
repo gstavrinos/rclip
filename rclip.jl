e43e029591eca1f78d30a21782cdf98f5209c9e7
George Stavrinos <stavrinosgeo@gmail.com>
2020-11-16 21:56:51 +0200

    First complete implementation

--- rclip.jl
+++ rclip.jl
@@ -19,53 +19,28 @@ function main()
         sort!(d, rev=true, lt=(x,y)->isless(length(x[1]),length(y[1])))
 
         i = 1
-        while true
+        while i <= length(d)
             pos = findfirst(d[i][1], clip_)
             if pos == nothing
                 i += 1
-                if i > length(d)
-                    break
-                end
             else
-                println(clip_)
-                println("BEFORE")
-                println(clip_)
-                println(clip)
-                println(d[i][1])
-                println(pos)
-                clip_ = replace(clip_, d[i][1] => repeat(PLACEHOLDER, length(d[i][2])), count=1)
-                #  _clip = collect(clip)
+                clip_ = replace(clip_, d[i][1] => repeat(PLACEHOLDER, sizeof(d[i][2])), count=1)
                 c = ""
-                #  deleteat!(_clip, pos)
-                p = pos[1]
                 if pos[1] > 1
-                    c = clip[1:pos[1]-sizeof(clip[pos[1]])]
-                end
-                while true
-                    if p > pos[length(pos)]
-                        break
-                    end
-                    c *= clip[p]
-                    p += sizeof(clip[p])
+                    c *= clip[1:prevind(clip, pos[1])]
                 end
                 c *= d[i][2]
-
-                if pos[length(pos)] < sizeof(clip)
-                    c *= clip[pos[1]+sizeof(d[i][1])-sizeof(d[i][1][1]):length(clip)]
+                replace_end = nextind(clip, last(pos))
+                if replace_end <= lastindex(clip)
+                    c *= clip[replace_end:lastindex(clip)]
                 end
-                #  insert!(_clip, p, collect(d[i][2]))
-                #  clip = String(_clip)
-                #  clip = replace(clip, d[i][1] => d[i][2], count=1)
                 clip = c
-                println("AFTER")
-                println(clip)
             end
         end
 
     end
 
-    #  clipboard(clip)
-    println(clip)
+    clipboard(clip)
 
 end
 
